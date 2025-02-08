47e30538cd7cb927bbf4791db4020bc25883765d
Yaosanqi137 <zxc18023571263@outlook.com>
2025-02-08 18:25:31 +0800

    致敬pepper(bushi

---
+++ 寒假写的/Pepper.cpp
@@ -0,0 +1,17 @@
+#define pepper std::cout
+#define Pepper main()
+#define pEpper <<
+#define pePper return
+#define pepPer ;
+#define P 0
+#define pep int
+#define p {
+#define e }
+#include <iostream>
+
+pep Pepper p
+    pepper pEpper "最爱 pepper" pepPer
+    pePper P pepPer
+e
+
+
